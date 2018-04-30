005e40100279486ba9856bbc9fbbf5dc92d55fab
Spencer Torres <spencerdtorres@gmail.com>
2018-04-29 21:23:15 -0400

    Read file from args. Run instructions. Cleaned typos and formatting.

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,26 +4,49 @@
 
 
 std::string loadTextFromFile(std::string filepath) {
-    std::ifstream stream(filepath);
-    std::string str((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
-    return str;
+	std::ifstream stream(filepath);
+	std::string str((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+	return str;
 }
 
-int main() {
+int main(int argc, char** argv) {
 
-    TokenStream stream;
-    stream.lex(loadTextFromFile("test.ds"));
+	// argv[1] will contain the file to read
+	if (!argv[1]) {
+		printf("Missing filename in args.");
+		return 1;
+	}
 
-    for (Token token: stream.tokens) {
-        printf("Type:%s, Raw: %s, Col: %d, Row: %d, Offset: %d\n", tokenTypeNames[(int)token.tokenType], token.raw.c_str(), token.column,
-               token.row, token.offset);
+	// Get file using argument as filepath
+	std::string fileContents = loadTextFromFile((std::string) argv[1]);
+	// If the string is empty, the file didn't read properly (or it's empty)
+	if (fileContents.empty()) {
+		printf("Error reading file: %s", argv[1]);
+		return 1;
+	}
+
+	TokenStream stream;
+	stream.lex(fileContents);
+
+	for (Token token: stream.tokens) {
+		printf("Type:%s, Raw: %s, Col: %d, Row: %d, Offset: %d\n",
+			tokenTypeNames[(int) token.tokenType],
+			token.raw.c_str(),
+			token.column,
+			token.row,
+			token.offset
+		);
     }
 
     for (Error error: stream.errors) {
-
-       printf("ErrorType:%d, Type:%d, Raw: %s, Col: %d, Row: %d, Offset: %d\n",
-               error.errorType, error.token.tokenType, error.token.raw.c_str(),
-               error.token.column, error.token.row, error.token.offset);
+		printf("ErrorType:%d, Type:%d, Raw: %s, Col: %d, Row: %d, Offset: %d\n",
+			error.errorType,
+			error.token.tokenType,
+			error.token.raw.c_str(),
+			error.token.column,
+			error.token.row,
+			error.token.offset
+		);
     }
 
     return 0;
