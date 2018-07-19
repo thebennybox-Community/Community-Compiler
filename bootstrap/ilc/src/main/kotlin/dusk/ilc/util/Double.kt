09bd0ba10d5ff11381305b0e5da922ae1c1091f0
myvarhd <myvarhd@gmail.com>
2018-07-19 21:05:47 +0200

    restart init

--- bootstrap/ilc/src/main/kotlin/dusk/ilc/util/Double.kt
+++
@@ -1,7 +0,0 @@
-package dusk.ilc.util
-
-import java.nio.ByteBuffer
-
-fun Double.toByteArray(): ByteArray {
-	return ByteBuffer.wrap(ByteArray(8)).putDouble(this).array()
-}
--- bootstrap/ilc/src/main/kotlin/dusk/ilc/util/Flags.kt
+++
@@ -1,6 +0,0 @@
-package dusk.ilc.util
-
-object Flags {
-	var optimization: Boolean = true
-	var verbose: Boolean = false
-}
